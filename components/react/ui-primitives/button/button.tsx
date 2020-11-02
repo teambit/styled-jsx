0a035851dbbb72d11c7fd8c31476a5ca68730ff1
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-11-02 12:27:19 +0200

    more updates

--- components/react/ui-primitives/button/button.tsx
+++ components/react/ui-primitives/button/button.tsx
@@ -1,6 +1,5 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import cs from 'classnames';
-import styles from './button.module.scss';
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   /**
@@ -11,59 +10,58 @@ export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button({ children, variant, ...rest }: IButton) {
   return (
-    <>
+    <div>
     <style jsx>{`
-        .base {
-          border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
-          padding: 10px;
-          color: #fff;
-          font-size: 16px;
-          font-family: 'Ubuntu', sans-serif;
-          font-weight: 700;
-          text-transform: uppercase;
-          outline: none;
-          transition: border-radius 0.5s;
-          &:active {
-            border-color: #313131;
-            background-color: #fff;
-            color: #000;
-          }
-          &:hover{
-            border-radius: 15px 225px 15px 255px/255px 15px 225px 15px;
-          }
-          &:disabled {
-            border-color: #a5a5a5;
-            background-color: #a5a5a5;
-            &:hover {
-              border-color: #a5a5a5;
-              background-color: #a5a5a5;
-              border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;;
-            }
-          }
-      }
-
-    .primary {
-      border: solid 7px #ab3636;
-      background-color: #ab3636;
-      &:hover {
-        border-color: #bb5151;
-        background-color: #bb5151;
-      }
-    }
-
-    .secondary {
-      border: solid 7px #2b3186;
-      background-color: #2b3186;
-      &:hover {
-        border-color: #5159cf;
-        background-color: #5159cf;
-      }
-    }
+    .base {
+	 border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+	 padding: 10px;
+	 color: #fff;
+	 font-size: 16px;
+	 font-family: 'Ubuntu', sans-serif;
+	 font-weight: 700;
+	 text-transform: uppercase;
+	 outline: none;
+	 transition: border-radius 0.5s;
+}
+ .base:active {
+	 border-color: #313131;
+	 background-color: #fff;
+	 color: #000;
+}
+ .base:hover {
+	 border-radius: 15px 225px 15px 255px/255px 15px 225px 15px;
+}
+ .base:disabled {
+	 border-color: #a5a5a5;
+	 background-color: #a5a5a5;
+}
+ .base:disabled:hover {
+	 border-color: #a5a5a5;
+	 background-color: #a5a5a5;
+	 border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+}
+ .primary {
+	 border: solid 7px #ab3636;
+	 background-color: #ab3636;
+}
+ .primary:hover {
+	 border-color: #bb5151;
+	 background-color: #bb5151;
+}
+ .secondary {
+	 border: solid 7px #2b3186;
+	 background-color: #2b3186;
+}
+ .secondary:hover {
+	 border-color: #5159cf;
+	 background-color: #5159cf;
+}
+ 
       `}</style>
-      <button className={cs(styles.base, styles[variant])} {...rest}>
+      <button className={cs('base', variant)} {...rest}>
         {children}
       </button>
-    </>
+    </div>
   );
 }
 
